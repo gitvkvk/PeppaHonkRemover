b4266d751992116a8a450d97dfeb4f7ff26113b8
kvk <kvk199819@gmail.com>
2022-12-15 22:59:41 -0600

    Added files for audio processing and detection

---
+++ pyautogui_script_peppa.py
@@ -0,0 +1,66 @@
+import pyautogui
+import os
+
+#os.startfile(r'C:\Users\kvk19\virtual\TestingENV\Samples')
+#fileList = os. listdir(r'C:\Users\kvk19\virtual\TestingENV\Samples') 
+#print (fileList)
+#for list in fileList:
+   # print(len(list))
+   # print(type(list)) 
+   # print(list[:-4]) #removing the .wav extension
+
+#if list is empty -> set value = 1
+
+#remove .wav
+#regular expression to extract the numbers
+#order list by greatest to least
+#save the greatest number to variable used within the clicking loop
+#provide an initial value if no files in folder or use a try/except
+
+
+#before doing the movements, wait until folder number is extracted
+
+#launch program, then hotkey for iterations, then close program
+
+
+#class here
+
+
+
+#pyautogui.displayMousePosition()
+
+pyautogui.moveTo(-1913,-140, duration = .1)
+pyautogui.click(-1913,-140)
+
+
+pyautogui.moveTo(-1733,0, duration = .1)
+pyautogui.click(-1733,0)
+
+pyautogui.moveTo(-1650,90, duration = .5)
+pyautogui.click(-1650,90)
+
+pyautogui.moveTo(-1600,230, duration = .5)
+#pyautogui.click(-1600,230)
+
+#control C
+#extract right most x didgets...leading zeros.... 001 for example
+#increment number while keeping leading zeros
+#paste output
+
+pyautogui.moveTo(-1400,240, duration = .5)
+
+pyautogui.moveTo(-900,520, duration = 1)
+
+#dd the point/clicking to delete OR shift k as below
+
+
+#pyautogui.keyDown('ctrl')
+
+#pyautogui.press('k')
+#pyautogui.keyUp('ctrl')
+
+
+
+#click play
+
+
